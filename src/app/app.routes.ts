d92dff42cf6b8e310ebbcb8084a96a1b335273e0
Dharsan <dharshankd57@gmail.com>
2025-12-03 23:14:03 +0530

    Describe your changes here

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Login } from './auth/login/login';
 import { Signup } from './auth/signup/signup';
@@ -6,8 +6,8 @@ import { Return } from './return/return';
 import { Home1 } from './admin/home1/home1';
 import { Edit } from './admin/edit/edit';
 import { AdminLogin } from './admin/admin-login/admin-login';
-
-
+import { NgModule } from '@angular/core';
+import { RouterModule, } from '@angular/router';
 
 export const routes: Routes = [
     {path: 'home', component: Home},
@@ -18,4 +18,12 @@ export const routes: Routes = [
     {path: 'edit', component: Edit},
     {path: 'admin-login', component: AdminLogin}
     
-]
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
+
